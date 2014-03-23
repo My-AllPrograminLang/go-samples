f6ef0ddf44830a85be95d48602dca0c55e67ec7d
Eli Bendersky <eliben@gmail.com>
2014-03-22 18:48:50 -0700

    Basic NextToken works, only for identifiers

---
+++ lexer/tblgen-lexer.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+type TokenName int
+
+// Values for TokenName
+const (
+	// Special tokens
+	EOF   TokenName = -1
+	ERROR           = iota
+	COMMENT
+	IDENTIFIER
+)
+
+type Token struct {
+	Name TokenName
+	Val  string
+	pos  int
+}
+
+type Lexer struct {
+	buf []byte
+
+	// Current rune.
+	r rune
+
+	// Position of the current rune in buf.
+	rpos int
+
+	// Position of the next rune in buf.
+	nextpos int
+}
+
+func NewLexer(buf []byte) *Lexer {
+	lex := Lexer{buf, -1, 0, 0}
+
+	// Prime the lexer by calling .next
+	lex.next()
+	return &lex
+}
+
+func (lex *Lexer) NextToken() Token {
+	lex.skipNontokens()
+
+	if lex.r < 0 {
+		return Token{EOF, "", lex.nextpos}
+	}
+
+	if isAlpha(lex.r) {
+		return lex.scanIdentifier()
+	} else {
+		return Token{ERROR, "", lex.rpos}
+	}
+}
+
+func (lex *Lexer) next() {
+	if lex.nextpos < len(lex.buf) {
+		lex.rpos = lex.nextpos
+
+		// r is the current rune, w is its width. We start by assuming the
+		// common case - that the current rune is ASCII (and thus has width=1).
+		r, w := rune(lex.buf[lex.nextpos]), 1
+
+		if r > utf8.RuneSelf {
+			// The current rune is not actually ASCII, so we have to decode it
+			// properly.
+			r, w = utf8.DecodeRune(lex.buf[lex.nextpos:])
+		}
+
+		lex.nextpos += w
+		lex.r = r
+	} else {
+		lex.rpos = len(lex.buf)
+		lex.r = -1 // EOF
+	}
+}
+
+func (lex *Lexer) skipNontokens() {
+	for lex.r == ' ' || lex.r == '\t' || lex.r == '\n' || lex.r == '\r' {
+		lex.next()
+	}
+}
+
+func (lex *Lexer) scanIdentifier() Token {
+	startpos := lex.rpos
+	for isAlpha(lex.r) || isDigit(lex.r) {
+		lex.next()
+	}
+	return Token{IDENTIFIER, string(lex.buf[startpos:lex.rpos]), startpos}
+}
+
+func isAlpha(r rune) bool {
+	return 'a' <= r && r <= 'z' || 'A' <= r && r <= 'Z' || r == '_'
+}
+
+func isDigit(r rune) bool {
+	return '0' <= r && r <= '9'
+}
+
+//------------------------------------------------------------------------------
+
+func main() {
+	//const sample = "本ähello  world"
+    const sample = "foo  \n bar baz"
+	fmt.Println(sample)
+
+	nl := NewLexer([]byte(sample))
+	fmt.Println(nl)
+
+    fmt.Println(nl.NextToken())
+    fmt.Println(nl.NextToken())
+    fmt.Println(nl.NextToken())
+    fmt.Println(nl.NextToken())
+    fmt.Println(nl.NextToken())
+}
