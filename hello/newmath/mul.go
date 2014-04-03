026ddea0f3a6fce4bd40c99194765e82df10e6cc
Eli Bendersky <eliben@gmail.com>
2014-04-03 06:29:56 -0700

    Show how unexported symbols can be used too - different files, same package

--- hello/newmath/mul.go
+++ hello/newmath/mul.go
@@ -3,3 +3,7 @@ package newmath
 func Mul(x, y float64) float64 {
 	return x * y
 }
+
+func minus(x, y float64) float64{
+	return x - y
+}
--- hello/newmath/sqrt.go
+++ hello/newmath/sqrt.go
@@ -5,7 +5,9 @@ package newmath
 func Sqrt(x float64) float64 {
 	z := 1.0
 	for i := 0; i < 1000; i++ {
-		z -= (z*z - x) / Mul(2, z)
+		// Can see both exported and non-exported symbols from mul.go, because
+		// it's all in the same package.
+		z -= minus(z*z, x) / Mul(2, z)
 	}
 	return z
 }
