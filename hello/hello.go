380fce183a16ffb5072b2680788c28dcaf0b8790
Eli Bendersky <eliben@gmail.com>
2014-04-08 07:22:12 -0700

    Example of importing package from a differently-named dir

--- hello/hello.go
+++ hello/hello.go
@@ -1,10 +1,16 @@
 package main
 
 import (
-    "fmt"
-    "go-samples/hello/newmath"
+	"fmt"
+	"go-samples/hello/newmath"
+
+	// dir name for the import
+	"go-samples/hello/pkgdir"
 )
 
 func main() {
 	fmt.Printf("Hello, world.  Sqrt(2) = %v\n", newmath.Sqrt(2))
+
+	// but package name is actually imported
+	somepkg.Something("202020")
 }
---
+++ hello/pkgdir/somepkg.go
@@ -0,0 +1,10 @@
+// Note: package name different from directory where it resides
+package somepkg
+
+import (
+	"fmt"
+)
+
+func Something(s string) {
+	fmt.Println(s)
+}
