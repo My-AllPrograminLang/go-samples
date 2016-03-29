fbf346689daed9f21396c313504ba50aab3d6c99
Eli Bendersky <eliben@gmail.com>
2016-03-28 17:09:11 -0700

    fetch sample with ex. 1.7

---
+++ gopl-exercises/ch1-fetch.go
@@ -0,0 +1,29 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// Fetch prints the content found at each specified URL.
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		fmt.Println(url)
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		// Exercise 1.7: use io.Copy
+		_, err = io.Copy(os.Stdout, resp.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Copy: %v\n", err)
+		}
+		resp.Body.Close()
+	}
+}
